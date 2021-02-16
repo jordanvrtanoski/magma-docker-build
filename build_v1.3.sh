d8d6f57dd1c6e551104d61976b8e4f686a068324
Jordan Vrtanoski <jordan.vrtanoski@gmail.com>
2021-02-16 23:07:20 +0400

    Adding adm64 support for v1.3.3

--- build_v1.3.sh
+++ build_v1.3.sh
@@ -1,13 +1,25 @@
-# Build Magma for aarch64
-docker build -t arm64v8/debian_magma:v1.3.3 -f arm64/magma.v1.3.3.Dockerfile .
-docker create --name magma arm64v8/debian_magma:v1.3.3
-docker cp magma:/packages packages/arm64
-docker rm magma
+## Build Magma for aarch64
+#docker build -t arm64v8/debian_magma:v1.3.3 -f arm64/magma.v1.3.3.Dockerfile .
+#docker create --name magma arm64v8/debian_magma:v1.3.3
+#docker cp magma:/packages packages/arm64
+#docker rm magma
 
 # Build OpenVSwitch for aarch64
-docker build -t arm64v8/debian_ovs:v1.3.3 -f arm64/ovs.v1.3.3.Dockerfile .
-docker create --name ovs arm64v8/debian_ovs:v1.3.3
-docker cp ovs:/packages packages/arm64
+#docker build -t arm64v8/debian_ovs:v1.3.3 -f arm64/ovs.v1.3.3.Dockerfile .
+#docker create --name ovs arm64v8/debian_ovs:v1.3.3
+#docker cp ovs:/packages packages/arm64
+#docker rm ovs
+
+## Build Magma for amd64
+docker build -t debian_magma:v1.3.3 -f amd64/magma.v1.3.3.Dockerfile .
+docker create --name magma debian_magma:v1.3.3
+docker cp magma:/packages packages/amd64
+docker rm magma
+
+# Build OpenVSwitch for amd64
+docker build -t debian_ovs:v1.3.3 -f amd64/ovs.v1.3.3.Dockerfile .
+docker create --name ovs debian_ovs:v1.3.3
+docker cp ovs:/packages packages/amd64
 docker rm ovs
 
 
